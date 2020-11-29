e3ca0ae224f734c0c01faa08c0b0556f8609a5c1
predo buzzi <pedro.buzzi@unidavi.edu.br>
2020-11-29 16:37:21 -0300

    parametros

--- Trabalho Final/bibliotecas/parametros.php
+++ Trabalho Final/bibliotecas/parametros.php
@@ -1,1 +1,6 @@
 <?php
+
+    define('ROOT_DIR',      dirname(__DIR__));
+    define('BIBLIOTECAS',   dirname(__DIR__) . '/bibliotecas/');
+    define('LAYOUTS',       dirname(__DIR__) . '/layouts/');
+    define('CADASTROS',     dirname(__DIR__) . '/cadastros/');
