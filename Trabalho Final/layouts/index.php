7b541230561b13d931b5f86afebb8f3f3e345d80
pedrobuzzJS <pedro.buzzi@unidavi.edu.br>
2020-11-29 18:18:20 -0300

    menu quase dinâmico

--- Trabalho Final/layouts/index.php
+++ Trabalho Final/layouts/index.php
@@ -1,2 +1,5 @@
 <?php
-echo 'certo';
+
+    require_once LAYOUTS . 'header.php';
+    require_once LAYOUTS . 'menu.php';
+    require_once LAYOUTS . 'footer.php';
---
+++ Trabalho Final/layouts/menu.php
@@ -0,0 +1,21 @@
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Web</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+            <ul class="navbar-nav mr-auto">
+                <li0 class="nav-item active">
+                    <a class="nav-link" href="index.php">Home <span class="sr-only">(página atual)</span></a>
+                </li0>
+                <?php
+
+                ?>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Sair</button>
+            </form>
+        </div>
+    </nav>
