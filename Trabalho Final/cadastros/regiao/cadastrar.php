c58c8c64c88190fae39ecfd8377deb2110125dd2
pedrobuzzJS <pedro.buzzi@unidavi.edu.br>
2020-12-05 00:12:01 -0300

    cadastro de região pronto

--- Trabalho Final/cadastros/regiao/cadastrar.php
+++ Trabalho Final/cadastros/regiao/cadastrar.php
@@ -1,17 +1,45 @@
 <?php
-    function formularioCadastrar(){
-       echo '<form method="post" action="">
-          <div class="form-group">
-            <label for="">Código</label>
-            <input type="number" class="form-control" id="" placeholder="código">
-          </div>   
-          <div class="form-group">
-            <label for="">Região</label>
-            <input type="text" class="form-control" id="" placeholder="código">
-          </div>       
-          <div class="form-group">
-            <label for="exampleFormControlTextarea1">Exemplo de textarea</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-          </div>
-        </form>';
+    function formularioRegiaoCastro(){
+        echo '<div class="col-8 col-sm-12">
+                  <form method="post" action="index.php?pg=regiao">                
+                  <div class="form-group">
+                    <label for="regiao">Regiao</label>
+                    <input type="text" class="form-control" name="regiao" id="regiao" placeholder="região">
+                  </div>                
+                  <div class="form-group">
+                  <input type="submit" class="btn btn-success" name="cadastrar" value="Cadastar">
+                  </div>  
+                </form>
+                </div>';
+    }
+
+    function cadastrarRegiao($conn) {
+        if (isset($_POST['cadastrar'])) {
+            try {
+                $sSql = "SELECT IDRegiao 
+                           FROM regiao
+                          ORDER BY IDRegiao DESC
+                          LIMIT 1;";
+
+                $stmt = $conn->prepare($sSql);
+                $stmt->execute();
+                $aResult = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                $uID = intval($aResult[0]['IDRegiao']) + 1;
+
+                $sSql = ' INSERT INTO regiao (IDRegiao, DescricaoRegiao)
+                          VALUES (:id, :nome)';
+
+                $stmt = $conn->prepare($sSql);
+
+                $stmt->execute([
+                    ':id'        => $uID,
+                    ':nome'      => $_POST['regiao']
+                ]);
+
+                header('Location: index.php?pg=regiao');
+
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
     }
--- Trabalho Final/cadastros/regiao/index.php
+++ Trabalho Final/cadastros/regiao/index.php
@@ -1,22 +1,30 @@
-<div class="justify-content-center mt-2">
-    <div class="justify-content-center">
-        <h1>Regiões</h1>
-    </div>
-    <div class="justify-content-center mt-2">
-        <form method="post" action="">
-            <label for="listar">Listar</label>
-            <input type="button" name="listar" id="listar" value="Exibir regiões">
-        </form>
+<div class="row justify-content-center mt-2">
+    <div class="justify-content-center my-2">
+        <h1>Categorias</h1>
     </div>
 </div>
 <div class="justify-content-center mt-2">
 
 <?php
-    require_once BIBLIOTECAS .  'conexao.php';
-    require_once CADASTROS .    'regiao/listar.php';
-    require_once CADASTROS .    'regiao/cadastrar.php';
+require_once BIBLIOTECAS . 'conexao.php';
+require_once CADASTROS . 'regiao/listar.php';
+require_once CADASTROS . 'regiao/cadastrar.php';
+formularioRegiaoCastro();
+listar($conn);
+cadastrarRegiao($conn);
 
-    if (!isset($_POST['listar'])) {
-        formularioCadastrar();
-        listar($conn);
+
+
+
+if (isset($_GET['acao'])) {
+    if ($_GET['acao'] == 'alterar') {
+        require_once CADASTROS . 'regiao/alterar.php';
+        $arrDados = catarCampos($conn);
+        formularioAlterar($arrDados[0]);
+        alterarRegiao($conn);
+        var_dump($_POST);
+    } elseif (($_GET['acao'] == 'excluir')) {
+        require_once CADASTROS . 'regiao/deletar.php';
+        deletarRegiao($conn);
     }
+}
