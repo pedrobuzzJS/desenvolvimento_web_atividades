c8f28520eb1b723382a36e1cc8860197b2522a18
pedrobuzzJS <pedro.buzzi@unidavi.edu.br>
2020-12-05 00:51:19 -0300

    deletar transportadora feita

--- Trabalho Final/cadastros/transportadora/deletar.php
+++ Trabalho Final/cadastros/transportadora/deletar.php
@@ -1,1 +1,16 @@
 <?php
+    function deletarTrasnportadora ($conn) {
+        try {
+            $sSql = 'DELETE FROM transportadoras WHERE IDTransportadora = :IDTRANS';
+            $stmt = $conn->prepare($sSql);
+
+            $stmt->execute([
+                ':IDTRANS' => $_GET['codigo']
+            ]);
+
+            header('Location: index.php?pg=transportadora');
+
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
