c9cebb909433bf278f94cad9ba8f0b200afb8e55
pedrobuzzJS <pedro.buzzi@unidavi.edu.br>
2020-12-05 03:56:22 -0300

    produto deletado

--- Trabalho Final/cadastros/produtos/index.php
+++
@@ -1,8 +0,0 @@
-<div class="row justify-content-center mt-2">
-    <div class="justify-content-center my-2">
-        <h1>Produtos</h1>
-    </div>
-<?php
-    require_once BIBLIOTECAS . 'conexao.php';
-    require_once CADASTROS . 'produtos/listar.php';
-    listar($conn);
