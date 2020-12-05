2df087ab40fb6caa518d1faa9d380b93fd34a1be
pedrobuzzJS <pedro.buzzi@unidavi.edu.br>
2020-12-05 00:19:36 -0300

    deletar regiao pronto

--- Trabalho Final/cadastros/regiao/deletar.php
+++ Trabalho Final/cadastros/regiao/deletar.php
@@ -1,1 +1,16 @@
 <?php
+    function deletarRegiao ($conn) {
+        try {
+            $sSql = 'DELETE FROM regiao WHERE IDRegiao = :IDREG';
+            $stmt = $conn->prepare($sSql);
+
+            $stmt->execute([
+                ':IDREG' => $_GET['codigo']
+            ]);
+
+            header('Location: index.php?pg=regiao');
+
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
